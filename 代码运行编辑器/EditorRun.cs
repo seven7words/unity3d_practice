6b787ed140c246da07acf04f22a9028602acfb4f
woopqww111 <woopqww111@yahoo.com>
2017-05-25 15:31:12 +0800

    代码扩展编辑器的开始暂停终止

---
+++ 代码运行编辑器/EditorRun.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+public class EditorRun  {
+
+    [MenuItem("Editor/Play")]
+    public static void Play(){
+        EditorApplication.isPlaying = true;
+	}
+	[MenuItem("Editor/Pause")]
+	public static void Pause()
+	{
+		EditorApplication.isPaused = true;
+	}
+	[MenuItem("Editor/Stop")]
+	public static void Stop()
+	{
+        EditorApplication.isPlaying = false;
+	}
+
+}
